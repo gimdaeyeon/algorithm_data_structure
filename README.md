4ad43e5ff0cfb46c4d7c7c73a86b8cd343e4664e
gimdaeyeon <rlaeodus1306@naver.com>
2024-03-02 01:13:39 +0900

    Study: 버블정렬1

---
+++ README.md
@@ -0,0 +1,2 @@
+# algorithm_data_structure
+알고리즘, 자료구조 공부, 연습을 위한 공간입니다.
--- src/sort/bubblesort/BubbleSort1.java
+++ src/sort/bubblesort/BubbleSort1.java
@@ -1,4 +1,33 @@
 package sort.bubblesort;
 
+import java.util.Arrays;
+
 public class BubbleSort1 {
+    /*  버블 정렬은 다음과 같은 순서로 이루어진다.
+      1. 나란히 위치한 두 요소(0번 인덱스, 1번 인덱스)를 비교하여 정렬한다.
+      2. 다음 순서의 두 요소(1번 인덱스, 2번 인덱스)를 비교하여 정렬한다.
+      3. 위와 같이 마지막 요소까지 비교하여 절렬한다.
+      4. 3번에서 정렬된 마지막 요소는 이제부터 비교 대상에서 제외된다.
+      5. 1~4번을 반복한다.
+      */
+    public static void main(String[] args) {
+        int[] arr = {7,5,23,4,7,8,1,2,5,6,8,3,1,45,6};
+        System.out.println(Arrays.toString(bubbleSort(arr)));
+    }
+
+    public static int[] bubbleSort(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 1; j < arr.length-i; j++) {
+                if(arr[j-1]>arr[j]){
+                    int tmp = arr[j-1];
+                    arr[j-1] = arr[j];
+                    arr[j] = tmp;
+                }
+            }
+        }
+
+        return arr;
+    }
+
+
 }
